b26b6e829d5a1d5158ecd0115a24851eb80629a9
Chaofan Zhang <chaofan.zhang@ef.com>
2019-03-20 11:11:54 +0800

    1. Add animations on parent, and query child to add child animations. 2. Animation callbacks. 3. Sequence animation with group() and stagger().

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,12 +1,27 @@
 import { Component} from '@angular/core';
 import { fade, slide, fadeInAnimation, slideInAnimation, fadeOutAnimation, slideOutAnimation } from 'app/animations';
-import { trigger, useAnimation, transition, style, animate } from '@angular/animations';
+import { trigger, useAnimation, transition, style, animate, query, animateChild, group, stagger } from '@angular/animations';
 
 @Component({
   selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
   animations: [
+    trigger('todosAnimation', [
+      transition(':enter', [
+        // To animate in parallel
+        group([
+          query('h1', [
+            style({ transform: 'translateY(-50px' }),
+            animate(1000)
+          ]),
+          // The parent ':enter' animation will block the child ':enter' animation,
+          // Need to invoke the animation mannually on its child.
+          query('@todoAnimation', stagger(200, animateChild())) 
+        ])
+      ])
+    ]),
+
     trigger('todoAnimation', [
       transition(':enter', [
         useAnimation(fadeInAnimation, {
@@ -38,5 +53,13 @@ export class TodosComponent {
   removeItem(item) {
     let index = this.items.indexOf(item);
     this.items.splice(index, 1);
+  }
+
+  onAnimationStart($event) {
+    console.log($event);
+  }
+
+  onAnimationDone($event) {
+    console.log($event);
   }
 }
